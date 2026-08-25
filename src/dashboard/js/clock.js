bfa2c64f0a966d98b6e4a9a05f1e1c8461cc4afb
Fabian Mäkilä <contact@fabianadrian.fi>
2026-08-25 21:43:04 +0300

    make clock js really small

--- src/dashboard/js/clock.js
+++ src/dashboard/js/clock.js
@@ -1,14 +1,5 @@
 const clockDisplay = document.getElementById("clock");
 
-function showTime(){
-    const date = new Date();
-
-    if (clockDisplay === null) {
-        return;
-    }
-
-    clockDisplay.textContent = date.toLocaleTimeString('sw-SE');
-    setTimeout(showTime, 1000);
-}
-
-showTime();
+setInterval(() => {
+  clockDisplay.textContent = new Date().toLocaleTimeString('sw-SE');
+}, 1000);
